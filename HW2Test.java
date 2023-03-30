a3a753521b5e84eec79ff313257b799510a65a95
Marion Skinner <marionlskinner@gmail.com>
2023-03-30 16:57:56 -0400

    Add files via upload

---
+++ HW2Test.java
@@ -0,0 +1,14 @@
+package cop2805;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HW2Test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
